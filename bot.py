174403ae087bcba2b4b852833fc10f5b8c4c4a43
Atalanta <42716753+AtaIanta@users.noreply.github.com>
2020-02-01 16:23:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,18 @@ class MyClient(discord.Client):
         # don't respond to ourselves
         if message.author == self.user:
             return
+        elif  message.content.startswith('$stats'):
+            url = 'https://bnonews.com/index.php/2020/01/the-latest-coronavirus-cases/' 
+            path = '//*[@id="mvp-content-main"]/table[1]/tbody/tr[33]/td[2]/strong'
+            path2 = '//*[@id="mvp-content-main"]/table[1]/tbody/tr[33]/td[3]/strong'
+            path3 = '//*[@id="mvp-content-main"]/table[1]/tbody/tr[33]/td[4]/strong/text()[3]'
+            response = requests.get(url) 
+            byte_data = response.content 
+            source_code = html.fromstring(byte_data)  
+            tree = source_code.xpath(path) + source_code.xpath(path2) + source_code.xpath(path3)
+            await message.channel.send('```'+tree[0].text_content() + ' chorych ' +tree[2].replace(" suspected","") + ' podejrzanych ' + tree[1].text_content() + ' zajebanych '+'```') 
 
-        if message.content == 'czy':
+        elif (message.content.startswith('Czy') or message.content.startswith('czy')) and message.channel.id == 660169524116717569:
             lista = ['Tak', 'Nie', 'Może', 'Zależy jak uważasz', 'Jeszcze jak', 'Nigdy w życiu','Twoja stara','ruchasz psa jak sra','Jak najbardziej','Możliwe że tak', 'Może nie','Nie wiem']
             await message.channel.send(random.choice(lista))
 
